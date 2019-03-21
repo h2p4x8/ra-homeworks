a9469c64b8d8430527725cefac105927e9c25fab
h2p4x8 <h2p4x8@gmail.com>
2019-03-21 23:02:07 +0300

    done

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function MessageHistory({list = []}) {
+  if (list.length == 0) {
+    return null;
+  }
+const history = list.map((message,mIndex) => {
+  switch(message.type) {
+  case 'message':
+    return <Message key={mIndex} from={message.from} message={message} />
+  break;
+
+  case 'response':
+    return <Response key={mIndex} from={message.from} message={message} />
+  break;
+
+  case 'typing':
+    return <Typing key={mIndex} from={message.from} message={message} />
+  break;
+}
+})
+  return (
+    <ul>
+      {history}
+    </ul>
+  )
+}
--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,14 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({count = 0}) {
+  if (count >= 1 && count <= 5){
+    const stars = [];
+    for (let i = 0; i < count; i++) {
+      stars.push(<li><Star /></li>)
+    }
+    return (
+      <ul className="card-body-stars u-clearfix">{stars}</ul>
+    )
+  }
+  return null;
 }
