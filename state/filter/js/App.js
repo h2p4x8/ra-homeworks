09bf1e5ac7d6f5bab3862e9b6650d78ab11c4bbd
h2p4x8 <h2p4x8@gmail.com>
2019-03-27 17:55:09 +0300

    done

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,5 +1,6 @@
 'use strict'
 
+/*
 const App = props => (
   <div>
     <Toolbar
@@ -9,3 +10,37 @@ const App = props => (
     <Portfolio projects={props.projects} />
   </div>
 );
+*/
+
+
+class App extends React.Component {
+  constructor ( props ) {
+    super(props);
+    this.state = {
+      selected: 'All'
+    };
+  }
+
+  selectedProjects() {
+    if (this.state.selected === 'All') {
+      return  this.props.projects;
+    }
+    return this.props.projects.filter(project => project.category === this.state.selected)
+  }
+
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.selected}
+          onSelectFilter={ (filter) => {
+            this.setState({
+                selected: filter
+              });
+            console.log(filter)}}/>
+        <Portfolio projects={this.selectedProjects()} />
+      </div>
+    )
+  }
+}
--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,15 +4,41 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewModIcon: VIEW_MODULE,
+      viewModModule: true
+    }
+  }
+
+  onSwitch() {
+    let icon, state;
+
+    if (this.state.viewModIcon === VIEW_MODULE) {
+      icon = VIEW_LIST;
+      state = false;
+    } else {
+      icon = VIEW_MODULE;
+      state = true;
+    }
+
+    this.setState({
+      viewModIcon: icon,
+      viewModModule: state
+    })
+    console.log("сменился тип вывода")
+  }
+
   render() {
     return (
       <div>
         <div className="toolbar">
           <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
+            icon={this.state.viewModIcon}
+            onSwitch={this.onSwitch.bind(this)} />
         </div>
-        {this.renderLayout(true)}
+        {this.renderLayout(this.state.viewModModule)}
       </div>
     );
   }
