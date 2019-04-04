e7f0624c4f0cabc860fbe7ebc581510a1401cf19
h2p4x8 <h2p4x8@gmail.com>
2019-04-04 21:48:58 +0300

    done2

--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,20 +1,29 @@
 'use strict';
 
 class SubscribeForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {email: '', errorClass : ''};
+  }
+
   checkEmail(e) {
-    if (e.target.validity.valid) {
-      this.form.classList.add('is-valid');
-      this.form.classList.remove('is-error');
-    } else {
-      this.form.classList.remove('is-valid');
-      this.form.classList.add('is-error');
+    let className = '';
+
+    if (e.target.value !== '') {
+      className = e.target.validity.valid ? 'is-valid' : 'is-error';
     }
+
+    this.setState({
+      errorClass: className,
+      email: e.target.value
+    })
   }
 
   render() {
+
     return (
       <div className="subscribe__form">
-        <form className="form form--subscribe" ref={el => this.form = el}>
+        <form className={`form form--subscribe ${this.state.errorClass}`}>
           <h4 className="form-title">Подписаться:</h4>
           <div className="form-group">
             <label htmlFor="input-email" className="sr-only">Email</label>
@@ -22,7 +31,8 @@ class SubscribeForm extends React.Component {
                    id="input-email"
                    placeholder="Email"
                    className="form-control"
-                   onInput={this.checkEmail.bind(this)}/>
+                   onInput={this.checkEmail.bind(this)}
+                   defaultValue={this.state.email}/>
             <div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
             <button type="submit" className="form-next">
               <i className="material-icons">keyboard_arrow_right</i>
